b7e583c20afbee4c61cd86f59491ee2817b2c96e
SmitaBidwaiD <smitabidwai2509@gmail.com>
2023-01-15 01:21:30 +0530

    edited as in main branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hii Smita");
-
+        System.out.println("In main branch");
     }
 }
